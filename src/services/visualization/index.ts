b64801ff25a6ba53f271e6c22ebc362302d1a79e
Riley Parsons <riley.s.parsons@gmail.com>
2021-09-01 21:31:59 -0700

    ENH: fix binning algorithm

--- src/services/visualization/index.ts
+++ src/services/visualization/index.ts
@@ -1,14 +1,14 @@
 import BigNumber from "bignumber.js";
 
-function findNearest(value: number, keys: number[]) {
+function findNearest(value: BigNumber, keys: BigNumber[]) {
   const nearest = keys.reduce((a, b) => {
-    const aDiff = Math.abs(a - value);
-    const bDiff = Math.abs(b - value);
+    const aDiff = a.minus(value).absoluteValue();
+    const bDiff = b.minus(value).absoluteValue();
 
-    if (aDiff === bDiff) {
-      return a > b ? a : b;
+    if (aDiff.isEqualTo(bDiff)) {
+      return a.isLessThan(b) ? a : b;
     } else {
-      return bDiff < aDiff ? b : a;
+      return bDiff.isLessThan(aDiff) ? b : a;
     }
   });
   return nearest;
@@ -23,49 +23,61 @@ const getBounds = (data: {[key: number]: number}) => {
 Splits each sample into divisions based on `split` parameter
 */
 export const binData = (data: {[key: number]: number}, binCount: number, split = 2) => {
-    const [minVolume, maxVolume] = getBounds(data);
+    let [_minVolume, _maxVolume] = getBounds(data);
+    const minVolume = new BigNumber(_minVolume);
+    const maxVolume = new BigNumber(_maxVolume);
 
-    const sampleIntervals = Object.keys(data).map((n) => +n);
+    const sampleIntervals = Object.keys(data).map((n) => new BigNumber(n)).sort((a, b) => a.minus(b) as any);
+
     const values = Object.values(data);
 
     const newData: any = {};
     for (let i=0; i < values.length; i++) {
-      newData[sampleIntervals[i]] = values[i];
+      newData[sampleIntervals[i].toString()] = new BigNumber(values[i]);
     }
 
-    const lastSample = Math.max(...sampleIntervals);
-    const firstSample = Math.min(...sampleIntervals);
+    const lastSample = sampleIntervals[sampleIntervals.length - 1];
+    const firstSample = sampleIntervals[0];
 
-    const sampleRange = lastSample - firstSample;
+    const sampleRange = lastSample.minus(firstSample).plus(sampleIntervals[1].minus(firstSample));
 
-    const sampleInterval = sampleRange / (sampleIntervals.length - 1);
+    const sampleInterval = sampleRange.dividedBy(sampleIntervals.length);
 
-    const binSize = +((lastSample - firstSample + sampleInterval) / binCount).toFixed(12);
-    const s = Math.floor(binSize / sampleInterval);
-    const obj: {[key: number]: number} = {};
-    for (let i = firstSample; i < lastSample + sampleInterval;) {
+    const binSize = lastSample.minus(firstSample).plus(sampleInterval).dividedBy(binCount);
+    
+    const s = binSize.dividedBy(sampleInterval).integerValue(BigNumber.ROUND_FLOOR);
+    const obj: {[key: string]: number} = {};
+
+    for (let i = new BigNumber(0); i.isLessThan(binCount);) {
+
+      const binStart = firstSample.plus(binSize.multipliedBy(i));
+
       const volumeSamples = [];
       
-      const nearest = findNearest(i, sampleIntervals);
+      const nearest = findNearest(binStart, sampleIntervals);
+
       let value;
 
-      if (s === 0) {
-        value = (newData[nearest] - minVolume) / (maxVolume - minVolume);
-      } else {
-        for (let j = i * sampleInterval; j <= i + s * sampleInterval; j += sampleInterval) {
-          volumeSamples.push((newData[nearest] - minVolume) / (maxVolume - minVolume));
+      let j = nearest
+
+      while (j.isLessThan(binStart.plus(binSize))) {
+        volumeSamples.push(
+          (newData[nearest.toString()].minus(minVolume)).dividedBy(maxVolume.minus(minVolume))
+        );
+        j = sampleIntervals[sampleIntervals.indexOf(j) +  1];
+
+        if (j == null) {
+          break;
         }
-        value = Math.max(...volumeSamples);
-      }
-      let z = 0
-
-      for (let k = i; k < +(i + binSize).toFixed(12);) {
-        obj[k] = value;
-        k = +(k + binSize/split).toFixed(12);
-        z++;
       }
 
-      i = +(i + binSize).toFixed(12);
+      value = BigNumber.max(...volumeSamples);
+
+      for (let k = binStart; k.isLessThan(binStart.plus(binSize));) {
+        obj[k.toString()] = value.toNumber();
+        k = k.plus(binSize.dividedBy(split));
+      }
+      i = i.plus(1);
     }
     return obj;
   }
